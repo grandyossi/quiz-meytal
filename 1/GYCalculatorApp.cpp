fdcc50ab65ad5503892ba425ce0d77a78f273c29
Yossi Schwartz <grandyossi@gmail.com>
2017-06-17 23:37:49 +0300

    Add files via upload

---
+++ 1/GYCalculatorApp.cpp
@@ -0,0 +1,149 @@
+#ifdef _WIN32
+#include<conio.h> //MS SPECIFIC
+#define gy_get_char _getche
+#else
+#include<iostream>
+#define gy_get_char getchar
+#endif //WIN
+#include"GYCalculatorApp.h"
+using namespace GYQuizMeytalCalculator;
+using std::queue;
+using std::stack;
+
+
+namespace {
+	//an empty queue of aritmetic chars.
+	//usage : ONLY as default arg (primary/main method invocation)
+	queue<char> fifoAritmeticzGlobal;
+} //anonymous [hidden] namespace
+
+
+//The current stack is about to return.
+//@returns calculations according to current queues
+static __int64 getStackNumbersArtimetics(
+						queue<__int64>& inNumbers,
+						queue<char>&	inAritmetics)
+{//TO_DO *1* : assert `n` numbers , `n-1` aritmetics ?
+	__int64 x, y, tempCalc = 0;
+	//inNumbers is a queue -> can't push tempCalcz to the queue!
+	bool isCalcMade = false;//++
+							//TO_DO *1* : assert NONZERO sized aritmeticz container
+	while (inNumbers.size() > 1 || isCalcMade)
+	{
+		if (isCalcMade)
+		{
+			x = tempCalc;
+			isCalcMade = false;
+		}
+		else
+		{
+			x = inNumbers.front();
+			inNumbers.pop();
+		}
+		y = inNumbers.front();
+		inNumbers.pop();
+		switch (inAritmetics.front())
+		{
+		case '+':
+			tempCalc = x + y; break;
+		case '-':
+			tempCalc = x - y; break;
+		case '/':
+			tempCalc = x / y; break; //TO_DO *3* : divide by zero ?
+		case '*':
+			tempCalc = x * y; break;
+		case '^':
+			tempCalc = (__int64)pow(x, y); break;
+		}
+		inAritmetics.pop();
+		if (inNumbers.size() == 0) break;//++
+		isCalcMade = true;//++
+						  //org : inNumbers.push(tempCalc);
+	}
+	return (inNumbers.size() == 0) ? tempCalc : inNumbers.front();
+}
+
+
+//-------------------------------------------------------------------------
+//recursive input of end-user chars via a static method
+//@returns current stack's calculation
+//-------------------------------------------------------------------------
+static	__int64 getCalculation(
+					queue<char>& inFifoPrevAritmicz = fifoAritmeticzGlobal)
+{
+	char			anInput = 0;
+	//true once `fifoDigitz` has elements
+	bool			isNumberInput = false;//number may be zero
+	__int64			theSign = 1, aVal = 0, tempNumber = 0;
+	//stream of digits
+	////////REM : not O(n)    queue<__int64>	fifoDigitz;
+	//steam of aritmetic chars
+	queue<char>		fifoAritmeticCharz;
+	//digits perform a number OR calculations result placeholder
+	queue<__int64>	lifoNumbers;
+	//
+	while (anInput != '\r' && anInput != '\n' && anInput != ')')
+	{
+		anInput = gy_get_char();
+		if ('(' == anInput)
+			lifoNumbers.push(
+				getCalculation(fifoAritmeticCharz)); //<- RECURSIVE
+		else
+		{//current stack
+			if (getIsCharArtimetic(anInput))
+				fifoAritmeticCharz.push(anInput);
+			else if ((aVal = getCharAsDigit(anInput))
+					 != GY_NOT_A_DIGIT)
+			{////////FIX :
+				if (tempNumber == 0)
+				{
+					isNumberInput = true;
+					tempNumber = aVal;
+				}
+				else
+				{
+					tempNumber *= 10;
+					tempNumber += aVal;
+				}
+				////////////fifoDigitz.push(aVal);
+			}
+			else if (' ' == anInput || '\r' == anInput ||
+				'\n' == anInput || ')' == anInput)
+			{//white space (std::ws) signifies a gap between NUMBERS :
+			 //2 1 (two and one) != 21 (twenty one).
+			 //ignore leading junk spaces <-> size > 0
+				////////REM : not O(n)
+				////////if (fifoDigitz.size() > 0)
+				////////{
+				////////	isNumberInput = true;//the digits may build a number
+				////////	aVal = 0;
+				////////}
+				////////while (fifoDigitz.size() > 0)
+				////////{//c++ power is... pow (not ^)
+				////////	aVal += fifoDigitz.front() *
+				////////		static_cast<__int64>(
+				////////			pow(10, fifoDigitz.size() - 1));
+				////////	fifoDigitz.pop();
+				////////}
+				if (isNumberInput)
+				{
+					lifoNumbers.push(theSign * tempNumber);
+					theSign = 1;
+					isNumberInput = false;
+					tempNumber = 0;
+				}
+			}
+			else if ('m' == anInput) theSign = -1;//TO_DO *4* : "mmmmX" ?
+		}//within current stack
+	}//while
+	return getStackNumbersArtimetics(
+		lifoNumbers,
+		(fifoAritmeticCharz.size() != 0)/*no aritmetic input in parenz?*/
+		? fifoAritmeticCharz : inFifoPrevAritmicz);
+} //getCalculation
+
+
+__int64 GYCalculatorApp::execute()
+{
+	return getCalculation();
+}
---
+++ 1/GYCalculatorApp.h
@@ -0,0 +1,59 @@
+//2017-06-16
+//	LOGIC :
+//	A.
+//		AT LEAST ONE arithmetic char before ANY digit char
+//	B.
+//		`n` numbers   <=>   `n-1` aritmetics
+//		digit_chars_count - 1   =   aritmetic_chars_count
+//	C.
+//		no floating point values as inputs
+//	D.
+//		no exception handling. Principal logic only
+//	E.
+//		implemented via a class in order to encapsulate some stuff,
+//		like an anonymous namespace which hides a global var...
+//
+#ifndef GY_MEYTAL_CALCULATOR
+#define GY_MEYTAL_CALCULATOR  
+#define BEGIN_GY_NS_MEYTAL_CALCULATOR namespace GYQuizMeytalCalculator {
+#define END_GY_NS_MEYTAL_CALCULATOR }
+#include<queue>
+#include<stack>
+BEGIN_GY_NS_MEYTAL_CALCULATOR
+#define GY_NOT_A_DIGIT -1
+
+
+//@returns true - once the char signifies an aritmetic op
+inline bool getIsCharArtimetic(const char& inC)
+{//Look, Mom, I made a prototype of a rocket out of... switch&cases
+ //#despicable_me_1
+	switch (inC)
+	{
+	case '+': case '-': case '/': case '*':	case '^': return true;
+	}
+	return false;
+}
+
+//, const __int64& theSign
+//A digit is a non-signed value,
+//thus a negative value means NAN.
+//@returns char converted to its numeric value, or GY_NOT_A_DIGIT
+inline __int64 getCharAsDigit(const char& inC)
+{//'0' - '0' = 0  ,  '1' - '0' = 1  ,  ...
+	if (inC >= '0' && inC <= '9') return (inC - '0');//(theSign) * 
+	//
+	return GY_NOT_A_DIGIT;
+}
+
+
+//Not a pure singleton, so let's call it an app :)
+//Instantiating this class is impossible
+struct GYCalculatorApp final 
+{
+	//Executes an app sequence : chars input, followed by final calculation
+	static __int64 execute();
+private:
+	GYCalculatorApp() = default;
+};
+END_GY_NS_MEYTAL_CALCULATOR
+#endif // !GY_MEYTAL_CALCULATOR
