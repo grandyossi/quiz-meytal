e19f6546a141bd8f4151e1435e353dc8dc6a744f
Yossi Schwartz <grandyossi@gmail.com>
2017-06-19 16:03:06 +0300

    Update main.cpp

--- 4/main.cpp
+++ 4/main.cpp
@@ -13,32 +13,33 @@ int g_i = 0;
 std::timed_mutex g_i_mutex = {};
 std::unique_lock<std::timed_mutex> g_i_mutex_lock = { g_i_mutex, std::defer_lock };
 //sai = simple atomic int
-std::atomic_int g_sai(0);
+std::atomic_int	g_sai(0);
+//whether incremented within `thProcTimedMutex`
+bool g_isMutexInc = false;
 //-------------------------------------------------------------
-//This method has an unresolved issue.
-//TO_DO *0* : why crash (no matter where `.unlock()` is)
-[[deprecated("This method has an unresolved issue")]]
+//Slow solution to the issue:
 void thProcTimedMutex()
 {
-	//shake things up via zzzleep and run:
-	std::this_thread::sleep_for(std::chrono::milliseconds(180));
 	if (g_i_mutex_lock.try_lock_for(std::chrono::milliseconds(18)))
 	{
+		g_isMutexInc = true;
+		//OK also :   int k = 0;
 		//std::timed_mutex has non-recursive ownership semantics:
 		//ONLY ONE lock can be acquired within the same thread
 		//(that's why the lock is not within the loop)
 		for (int k = 0; k < 10; ++k) ++g_i;
-		//NOTICE : k MUST be declared WITHIN the `for`
-		//g_i_mutex_lock.unlock();
+		g_i_mutex_lock.unlock();
 	}//try lock
-	if (g_i_mutex_lock.owns_lock()) g_i_mutex_lock.unlock();
+	//OK, but REM:  if (g_i_mutex_lock.owns_lock()) g_i_mutex_lock.unlock();
 }
+//Primary (fastest) solution to the issue:
 void thProcAtomic()
 {
 	//shake things up, with style, via zzzleep and run:
 	std::this_thread::sleep_for(std::chrono::milliseconds(18));
 	for (int k = 0; k < 10; ++k) ++g_sai;
 }
+//ISSUE : incremented value of `g_i` is unknow
 void thProc()
 {
 	//shake things up, with style, via zzzleep and run:
@@ -49,14 +50,18 @@ void doShakeThingzUp(std::function<void ()> inThreadProc)
 {
 	g_i = 0;
 	g_sai = 0;
+	g_isMutexInc = false;
 	std::vector<std::thread> vTh;
+	//TO_DO *1* : why sleep inside `thProcTimedMutex` causes crash?
+	//shake things up via zzzleep and run:
+	std::this_thread::sleep_for(std::chrono::milliseconds(180));
 	for (int i = 0; i < 18; ++i)
 		vTh.emplace_back(inThreadProc);
 	for (auto& th : vTh)
 		th.join();
 	//ONE main thread rules here:
-	cout << ((g_i > 0) ? "Unsafe" : "Atomic") << " increment :   ";
-	cout << ((g_i > 0) ? g_i : g_sai) << "\n";
+	cout << (g_i > 0 ? (g_isMutexInc ? "Mutex" : "Unsafe") : "Atomic") << " increment :   ";
+	cout << (g_i > 0 ? g_i : g_sai) << "\n";
 }
 //============================================
 int main()
@@ -70,6 +75,7 @@ int main()
 	doShakeThingzUp(thProcAtomic);
 	doShakeThingzUp(thProcAtomic);
 	doShakeThingzUp(thProcAtomic);
+	cout << "\nHit Enter...\n";
 	getchar();
 	return 0;
 }
